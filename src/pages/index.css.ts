6f7188727c5dae9f46a3a0a37b6aca0a382bb51a
Mitchell Busby <mitchbusby@gmail.com>
2024-10-09 19:06:21 +1100

    finish homepage

--- src/pages/index.css.ts
+++ src/pages/index.css.ts
@@ -5,3 +5,15 @@ export const wrapper = style({
   flexDirection: "column",
   gap: "24px",
 });
+
+export const list = style({
+  columnCount: 1,
+  "@media": {
+    "(min-width: 400px)": {
+      columnCount: 2,
+    },
+    "(min-width: 700px)": {
+      columnCount: 3,
+    },
+  },
+});
